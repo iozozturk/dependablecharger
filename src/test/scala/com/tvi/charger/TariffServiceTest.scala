249988ee30a610bc2a78f9f4053e6bd285cf2df1
ismet ozozturk <iozozturk@gmail.com>
2019-03-31 00:14:12 +0100

    Tariff service unit test refactoring

--- src/test/scala/com/tvi/charger/TariffServiceTest.scala
+++ src/test/scala/com/tvi/charger/TariffServiceTest.scala
@@ -4,65 +4,35 @@ import java.time.Instant
 import java.util.Currency
 
 import akka.actor.ActorSystem
-import akka.stream.ActorMaterializer
 import com.tvi.charger.models._
-import org.scalatest.{BeforeAndAfterEach, Matchers, WordSpec}
+import org.mockito.Mockito.when
+import org.scalatest.{Matchers, WordSpec}
 import org.scalatestplus.mockito.MockitoSugar
 
-class TariffServiceTest extends WordSpec with Matchers with MockitoSugar with BeforeAndAfterEach {
-  implicit val actorSystem: ActorSystem = ActorSystem("dress-pipeline")
-  implicit val materializer: ActorMaterializer = ActorMaterializer()
-  val tariffServiceInTest = new TariffService()
-
-  override protected def beforeEach(): Unit = {
-    fixture.cleanTariffState()
-  }
+class TariffServiceTest extends WordSpec with Matchers with MockitoSugar {
+  private implicit val actorSystem: ActorSystem = ActorSystem()
+  private val mockTariffRepo = mock[TariffRepository]
+  private val tariffServiceInTest = new TariffService(mockTariffRepo)
 
   "TariffServiceTest" should {
 
     "save new tariff" in {
       val tariff = Tariff(EnergyFee(1), Some(ParkingFee(1)), ServiceFee(0.1), Currency.getInstance("EUR"), Instant.parse("2119-03-27T00:00:50Z"), User("ismet"))
-      tariffServiceInTest.save(tariff) shouldEqual TariffSaveResult(success = true, None)
+      val saveResult = TariffSaveResult(success = true, None)
+
+      when(mockTariffRepo.save(tariff)) thenReturn saveResult
+      tariffServiceInTest.save(tariff) shouldEqual saveResult
     }
 
-    "respond with not saved for new tariff with duplicate existing startdate" in {
+    "get tariff for the time" in{
+      val now = Instant.now
       val tariff = Tariff(EnergyFee(1), Some(ParkingFee(1)), ServiceFee(0.1), Currency.getInstance("EUR"), Instant.parse("2119-03-27T00:00:50Z"), User("ismet"))
-      tariffServiceInTest.tariffs += (tariff.startDate -> tariff)
-      val tariff2 = Tariff(EnergyFee(1), Some(ParkingFee(1)), ServiceFee(0.1), Currency.getInstance("EUR"), Instant.parse("2119-03-27T00:00:50Z"), User("ismet"))
-      tariffServiceInTest.save(tariff2) shouldEqual TariffSaveResult(success = false, Some("tariff with the start date already exist. tariff owner=ismet"))
+
+      when(mockTariffRepo.getClosestTariff(now)) thenReturn tariff
+      tariffServiceInTest.getTariff(now) shouldEqual tariff
     }
 
-    "find closest tariff to date" in {
-      Seq(
-        Tariff(EnergyFee(1), Some(ParkingFee(1)), ServiceFee(0.1), Currency.getInstance("EUR"), Instant.parse("2119-03-27T00:00:50Z"), User("ismet")),
-        Tariff(EnergyFee(2), Some(ParkingFee(2)), ServiceFee(0.2), Currency.getInstance("EUR"), Instant.parse("2119-03-25T00:00:50Z"), User("merve")),
-        Tariff(EnergyFee(3), Some(ParkingFee(3)), ServiceFee(0.3), Currency.getInstance("EUR"), Instant.parse("2119-03-23T00:00:50Z"), User("elon")),
-        Tariff(EnergyFee(3), Some(ParkingFee(3)), ServiceFee(0.3), Currency.getInstance("EUR"), Instant.parse("2119-03-21T00:00:50Z"), User("elon")),
-        Tariff(EnergyFee(3), Some(ParkingFee(3)), ServiceFee(0.3), Currency.getInstance("EUR"), Instant.parse("2119-03-19T00:00:50Z"), User("elon")),
-        Tariff(EnergyFee(3), Some(ParkingFee(3)), ServiceFee(0.3), Currency.getInstance("EUR"), Instant.parse("2119-03-19T00:00:49Z"), User("elon")),
-      ).foreach { tariff =>
-        tariffServiceInTest.tariffs += (tariff.startDate -> tariff)
-      }
 
-      val tariff = tariffServiceInTest.findTariff(Instant.parse("2119-03-26T00:00:50Z"))
-      tariff.startDate shouldEqual Instant.parse("2119-03-25T00:00:50Z")
-
-      val tariff2 = tariffServiceInTest.findTariff(Instant.parse("2119-03-20T00:00:50Z"))
-      tariff2.startDate shouldEqual Instant.parse("2119-03-19T00:00:50Z")
-
-      val tariff3 = tariffServiceInTest.findTariff(Instant.parse("2119-03-30T00:00:50Z"))
-      tariff3.startDate shouldEqual Instant.parse("2119-03-27T00:00:50Z")
-
-      val tariff4 = tariffServiceInTest.findTariff(Instant.parse("2119-03-19T00:00:49Z"))
-      tariff4.startDate shouldEqual Instant.parse("2119-03-19T00:00:49Z")
-    }
-
-  }
-
-  object fixture {
-    def cleanTariffState(): Unit = {
-      tariffServiceInTest.tariffs.clear()
-    }
   }
 
 }
